5be8a2880e3f1f875593fbc919bd99eca51f1985
AdminAkai <akatoirotech@gmail.com>
2024-09-13 04:17:11 -0400

    Added jsdocs to ScrambleText and span styling

--- lib/ScrambleText/ScrambleText.tsx
+++ lib/ScrambleText/ScrambleText.tsx
@@ -1,14 +1,21 @@
-import { FC } from 'react'
+/**
+ * ScrambleText is a freely customizable scramble-text effect.
+ * @param {string} text - Text to scramble.
+ */
+import { DetailedHTMLProps, FC, HTMLAttributes } from 'react'
 import { useHackerScramble } from '../hooks/stringHooks'
 
-type ScrambleTextProps = {
+type ScrambleTextProps = DetailedHTMLProps<
+  HTMLAttributes<HTMLSpanElement>,
+  HTMLSpanElement
+> & {
   text: string
 }
 
-const ScrambleText: FC<ScrambleTextProps> = ({ text }) => {
+const ScrambleText: FC<ScrambleTextProps> = ({ text, ...rest }) => {
   const scrambledText = useHackerScramble(text)
 
-  return <span>{scrambledText}</span>
+  return <span {...rest}>{scrambledText}</span>
 }
 
 export default ScrambleText
